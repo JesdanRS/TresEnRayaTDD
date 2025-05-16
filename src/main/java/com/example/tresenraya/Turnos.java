d0441556ed61794bd0799f215f3ccd605a198dac
DamaNiLe <damarisninaleonardini@gmail.com>
2025-05-15 22:58:54 -0400

    Haciendo fallar primer test - El primer turno siempre debe ser jugado por ‘X’

--- src/main/java/com/example/tresenraya/Turnos.java
+++ src/main/java/com/example/tresenraya/Turnos.java
@@ -3,6 +3,6 @@ package com.example.tresenraya;
 public class Turnos {
     public Ficha obtenerJugadorActual() {
         // Retornamos O para hacer fallar el test inicialmente
-        return Ficha.X;
+        return Ficha.O;
     }
 }
