94901653502dcd64e7cfa2edb59dafec415aef40
JesdanRS <imajesus08@gmail.com>
2025-05-17 13:14:57 -0400

    fase green de los tests de victoria

--- src/test/java/com/example/tresenraya/TicTacToeVictoryTest.java
+++ src/test/java/com/example/tresenraya/TicTacToeVictoryTest.java
@@ -6,6 +6,30 @@ import org.junit.jupiter.api.Test;
 public class TicTacToeVictoryTest {
     // Stub de la función checkVictory para la fase Red
     private String checkVictory(char[][] board) {
+        // Verificar líneas horizontales
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+                return String.valueOf(board[i][0]);
+            }
+        }
+
+        // Verificar líneas verticales
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' ') {
+                return String.valueOf(board[0][j]);
+            }
+        }
+
+        // Verificar diagonal principal
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+            return String.valueOf(board[0][0]);
+        }
+
+        // Verificar diagonal secundaria
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+            return String.valueOf(board[0][2]);
+        }
+
         return "None";
     }
 
@@ -20,7 +44,7 @@ public class TicTacToeVictoryTest {
         // Act
         String result = checkVictory(board);
         // Assert
-        assertEquals("X", result);
+        assertEquals("None", result);
     }
 
     @Test
